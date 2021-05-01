cac20db68a1ff51ab98d4868c4e904b2cad1c61a
mariu anchundia <mariuxi_secundaria@hotmail.com>
2021-04-30 23:54:15 -0500

    Agregar archivos de proyecto.

---
+++ AnchundiaMari_Examen/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnchundiaMari_Examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String sUsuario = txtUsuario.Text;
+            String sPassword = txtPassword.Text;
+
+            if ((sUsuario == "estudiante2021") && (sPassword == "uisrael2021"))
+            {
+                Navigation.PushAsync(new Registro());
+
+            }
+            else
+
+            {
+                lblResultado.Text =  "Dato incorrecto." ;
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ AnchundiaMari_Examen/Registro.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnchundiaMari_Examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //
+            //var monto = double.Parse(Monto.Text);
+            //var pago = double.Parse(Pago.Text);
+            //var iva = double.Parse("");
+
+//           var mensual =  pago-monto /(3*iva);
+
+           
+                
+            
+          
+
+
+        }
+
+        
+    }
+}
